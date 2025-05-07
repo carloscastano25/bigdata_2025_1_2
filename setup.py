413930b31647ba4bbe5f2cd018a1a6b4ac86914d
carloscastano25 <118127707+carloscastano25@users.noreply.github.com>
2025-05-07 13:14:38 +0000

    Corrección dependencias

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ setup(
     author="Carlos Dueiner",
     author_email="carlos.castano@est.iudigital.edu.co",
     description="Base de datos de COVID-19 en Antioquia",
-    version="0.1",
-    packages=["src/big_data", "src/big_data.static"],
-    package_dir={"": "src"},
-    package_data={
-        "src/big_data": ["static/db/*.db", "static/csv/*.csv"],
-        "src/big_data.static": ["dataexcel.py", "database.py", "main.py"]
-    },
     install_requires=[
         "pandas",
         "openpyxl",
